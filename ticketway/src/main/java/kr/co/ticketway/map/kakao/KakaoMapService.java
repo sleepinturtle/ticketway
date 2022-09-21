8ca17db3d69c830d761a04d72c2676bcfbd7aba3
sleepinturtle <96879024+sleepinturtle@users.noreply.github.com>
2022-09-21 19:35:37 +0900

    2022-09-21 bki 상품 마무리

--- ticketway/src/main/java/kr/co/ticketway/map/kakao/KakaoMapService.java
+++ ticketway/src/main/java/kr/co/ticketway/map/kakao/KakaoMapService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kr.co.ticketway.util.dto.MemberDTO;
+import kr.co.ticketway.perform.PerformDTO;
 
 @Service
 public class KakaoMapService {
@@ -13,10 +13,10 @@ public class KakaoMapService {
 	@Autowired
 	private KakaoMapDAO dao;
 
-	public List<MemberDTO> selectAllMember() {
-		List<MemberDTO> list = null;
-		list = dao.selectAllMember();
-		return list;
+	public PerformDTO selectTht() {
+		PerformDTO dto= null;
+		dto = dao.selectTht();
+		return dto;
 	}//selectAllMember
 
 }//class
--- ticketway/src/main/java/kr/co/ticketway/perform/PerformController.java
+++ ticketway/src/main/java/kr/co/ticketway/perform/PerformController.java
@@ -1,6 +1,9 @@
 package kr.co.ticketway.perform;
 
+import java.io.PrintWriter;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.co.ticketway.util.dto.MemberDTO;
 
 
 
@@ -41,17 +46,35 @@ public class PerformController {
 	}//list
 	
 	@RequestMapping(value = "/detail", method = RequestMethod.GET)
-	public String detail(Model model, String play_no) {
+	public String detail(Model model, String play_no, HttpSession session) {
 		PerformDTO dto = null;
+		PerformDTO dto2 = null;
+		List<ReviewDTO> dto1 = null;
 		dto = service.detail(play_no);
+		dto1 = service.review(play_no);
+		dto2 = service.thtmap(play_no);
 		model.addAttribute("play_info", dto);
+		model.addAttribute("review_info", dto1);
+		model.addAttribute("tht", dto2);
 		return "/perform/detail";
+	}
+	
+	@RequestMapping(value = "/cmt_insert", method= RequestMethod.POST)
+	public void comment(ReviewDTO dto, HttpSession session, PrintWriter out) {
+		dto.setMno( ( (MemberDTO) session.getAttribute("login_info") ).getMno() );
+		int successCount = 0;
+		successCount = service.replyInsert( dto );
+
+		out.print(successCount);
+		out.close();
+		
 	}
 	
 	@RequestMapping(value = "/prototype", method = RequestMethod.GET)
 	public String test() {
 		return "/prototype";
 	}
+	
 	
 	
 	
